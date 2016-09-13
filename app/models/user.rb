5d16f706d695b0e7f1c709273715d1debb56a859
Jesse Novotny <jessenovotny@gmail.com>
2016-09-13 12:51:24 -0700

    added upvoted_answers through upvotes in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
   has_many :questions
   has_many :answers
   has_many :upvotes
+  has_many :upvoted_answers, through: :upvotes
   validates :username, presence: true, uniqueness: true
   has_secure_password
+
+  def most_upvoted
+    binding.pry
+    upvoted_answers.uniq.sort.first
+  end
 end
