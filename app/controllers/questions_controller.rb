1b14ebef9825116b9868818dfd4346e52208f3d7
Jesse Novotny <jessenovotny@gmail.com>
2016-09-12 17:11:30 -0700

    cleaned up Questions Controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,52 +1,40 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
-  # GET /questions
-  # GET /questions.json
+
   def index
     @questions = Question.all
   end
 
-  # GET /questions/1
-  # GET /questions/1.json
+
   def show
   end
 
-  # GET /questions/new
   def new
     @question = Question.new
   end
 
-  # GET /questions/1/edit
   def edit
   end
 
-  # POST /questions
-  # POST /questions.json
   def create
     @question = Question.new(question_params)
 
     respond_to do |format|
       if @question.save
-        format.html { redirect_to @question, notice: 'Question was successfully created.' }
-        format.json { render :show, status: :created, location: @question }
+        redirect_to @question, notice: 'Question was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
+        render :new
       end
     end
   end
 
-  # PATCH/PUT /questions/1
-  # PATCH/PUT /questions/1.json
   def update
     respond_to do |format|
       if @question.update(question_params)
-        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
-        format.json { render :show, status: :ok, location: @question }
+        redirect_to @question, notice: 'Question was successfully updated.'
       else
-        format.html { render :edit }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
+        render :edit
       end
     end
   end
@@ -56,8 +44,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question.destroy
     respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
-      format.json { head :no_content }
+      redirect_to questions_url, notice: 'Question was successfully destroyed.'
     end
   end
 
