456ab8a5caadf0a1fd36551c13670f5cc32b671b
Jesse Novotny <jessenovotny@gmail.com>
2016-09-14 11:49:20 -0700

    added new answer form and controller actions to create answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,11 @@
 class AnswersController < ApplicationController
 
   def new
+    @question = Question.find(params[:question_id])
+    if current_user == @question.asker
+      flash[:error] = "You cannot answer your own question"
+      return redirect_to :back
+    end
     @answer = Answer.new
   end
 
