05e1a9100bf3b3906d4099d514664b25abb6f13e
Jesse Novotny <jessenovotny@gmail.com>
2016-09-14 09:40:56 -0700

    added button_to upvote answer on questions show/index

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -1,5 +1,7 @@
 class UpvotesController < ApplicationController
   def create
+    binding.pry
+    Answer.find(params[:answer_id]).upvotes.build(voter_id: current_user.id).save
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
 
   resources :questions, only: [:show, :new, :create, :edit, :update, :destroy] do
     resources :answers, only: [:new, :create, :edit, :update, :destroy]
-    # resources :upvotes, only: [:create, :destroy]
   end
 
   resources :answer, only: [] do
