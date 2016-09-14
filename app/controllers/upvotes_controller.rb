3cf100afbabeed6bbf54f01c486c247b964c956e
Jesse Novotny <jessenovotny@gmail.com>
2016-09-14 12:50:54 -0700

    can now remove upvote by clicking again

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -1,7 +1,11 @@
 class UpvotesController < ApplicationController
+  
   def create
-    # binding.pry
-    Answer.find(params[:answer_id]).upvotes.build(voter_id: current_user.id).save
+    upvote = Answer.find(params[:answer_id]).upvotes.build(voter_id: current_user.id)
+    unless upvote.save
+      Upvote.find_by(id: upvote.errors.messages[:answer][0]).try(:delete)
+      flash[:error] = upvote.errors.messages[:voter] unless upvote.errors.messages[:user].empty?
+    end
     redirect_to :back
   end
 
--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -2,11 +2,17 @@ class Upvote < ApplicationRecord
   belongs_to :voter, class_name: 'User'
   belongs_to :answer
   validates :voter, :answer, presence: true
-  validate :no_self_upvote
+  validate :no_self_upvote, :no_spam_upvote
 
   def no_self_upvote
     if voter == answer.user
       errors.add(:voter, "cannot upvote your own answer.")
+    end
+  end
+
+  def no_spam_upvote
+    if upvote = Upvote.find_by(voter_id: voter_id, answer_id: answer_id)
+      errors.add(:answer, "#{upvote.id}")
     end
   end
 
