f8c5a53063752c71800c0686c1f26c3b120b9e5b
Jesse Novotny <jessenovotny@gmail.com>
2016-09-14 10:19:04 -0700

    configured login page with form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
     binding.pry
-    
+
     # user = User.find_or_create_by(:uid => auth['uid']) do |u|
     #   u.name = auth['info']['name']
     #   u.email = auth['info']['email']
@@ -14,6 +15,9 @@ class SessionsController < ApplicationController
   end
  
   def destroy
+    session.destroy
+    flash[:message] = "Successfully logged out."
+    redirect_to root_path
   end
 
   private
