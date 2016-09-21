6dead3c31bb31ab01e794fefe1456fbb75a32e31
Jesse Novotny <jessenovotny@gmail.com>
2016-09-21 09:56:51 -0700

    trying different key variable declarations

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_APP_SECRET, FACEBOOK_APP_ID
+  provider :facebook, ENV['FACEBOOK_APP_SECRET'], ENV['FACEBOOK_APP_ID']
 end
 
