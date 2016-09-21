3c3f70e25c48ac4b836650f788690421d3a73a8b
Jesse Novotny <jessenovotny@gmail.com>
2016-09-21 11:09:10 -0700

    adjusted seed file and fixed index partial locals for category to header

--- db/schema.rb
+++ db/schema.rb
@@ -38,9 +38,9 @@ ActiveRecord::Schema.define(version: 20160915005251) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string  "username"
-    t.string  "password_digest"
-    t.integer "uid",             limit: 32
+    t.string "username"
+    t.string "password_digest"
+    t.string "uid"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,42 @@ end
 
 ### SEED USERS ####
 
-10.times do
+30.times do
   User.create(username: Faker::Internet.user_name, password: "password")
 end
 
 ### SEED QUESTIONS PER USER ####
 
-30.times do
-  User.find(rand(1..10)).questions.build(content: Faker::StarWars.quote).save
+40.times do
+  User.find(rand(1..30)).questions.build(content: Faker::StarWars.quote).save
 end
 
 ### SEED CATEGORIES & ANSWERS PER QUESTION ####
 
 Question.all.each do |question|
-  question.categories << Category.find(rand(1..5))
-  question.answers.build(content: Faker::Hacker.say_something_smart, user_id: rand(1..5)).save
-  question.answers.build(content: Faker::Hacker.say_something_smart, user_id: rand(1..5)).save if rand(1..3).odd?
-  question.answers.build(content: Faker::Hacker.say_something_smart, user_id: rand(1..5)).save if rand(1..3).odd?
+  4.times do
+    category = Category.find(rand(1..10))
+    question.categories << category unless question.categories.include?(category)
+  end
+
+  10.times do
+    question.answers.build(content: Faker::Hacker.say_something_smart, user_id: rand(1..10)).save if rand(1..3).odd?
+  end
 end
 
 ### SEED UPVOTES PER ANSWER OF TOP SEARCH RESULT FILTERS ####
-100.times do
-  Question.newest.take(3).each{|question| question.answers.each{|answer| answer.upvotes.build(voter_id: User.find_by(id: rand(1..5)).id).save}}
-
-  Question.oldest.take(3).each{|question| question.answers.each{|answer| answer.upvotes.build(voter_id: User.find_by(id: rand(1..5)).id).save}}
-
-  Question.most_answered.take(3).each{|question| question.answers.each{|answer| answer.upvotes.build(voter_id: User.find_by(id: rand(1..5)).id).save}}
+Question.all.each do |question| 
+  question.answers.each do |answer| 
+    10.times do 
+      answer.upvotes.build(voter_id: User.find_by(id: rand(1..30)).id).save if rand(1..2).odd?
+    end
+  end
 end
+
+# 100.times do
+#   Question.oldest.each{|question| question.answers.each{|answer| answer.upvotes.build(voter_id: User.find_by(id: rand(1..20)).id).save}}
+# end
+
+# 100.times do
+#   Question.most_answered.each{|question| question.answers.each{|answer| answer.upvotes.build(voter_id: User.find_by(id: rand(1..20)).id).save}}
+# end
