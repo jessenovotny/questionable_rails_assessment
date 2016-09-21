bc1ec1ffe0d76687094a9848eb21df7d6d3aed14
Jesse Novotny <jessenovotny@gmail.com>
2016-09-21 15:48:43 -0700

    pushing new files for Heroku migration

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,15 @@
+class FavoritesController < ApplicationController
+
+  def index
+    # binding.pry
+    @user = User.find(params[:user_id])
+    @questions = @user.favorited_questions
+  end
+
+  def create
+    question = Question.find(params[:question_id])
+    favorite = question.favorites.build(user_id: current_user.id)
+    Favorite.find_by(id: favorite.errors.messages[:question]).try(:delete) unless favorite.save
+    redirect_to :back
+  end
+end
---
+++ app/models/favorite.rb
@@ -0,0 +1,13 @@
+class Favorite < ApplicationRecord
+  belongs_to :question
+  belongs_to :user
+
+  validate :no_spam_favorite
+
+  def no_spam_favorite
+    if favorite = Favorite.find_by(user_id: user_id, question_id: question_id)
+      errors.add(:question, "#{favorite.id}")
+    end
+  end
+
+end
